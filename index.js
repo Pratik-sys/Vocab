fc2769d4a9bbc63536ed5c83cf6909680efd8c05
Pratik-sys <pithore.pratik@gmail.com>
2023-07-26 18:07:43 +0530

    use dotenv

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const vocabroute = require("./routes/index")
-
+require("dotenv").config();
 
 app.use("/", vocabroute);
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log("listening on port 3000")
 })
